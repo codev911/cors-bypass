ce3b2a5c3540c09cf34316d74ad888e3636bde8c
Geraldine Adi <53482087+codev911@users.noreply.github.com>
2022-03-08 15:23:43 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,43 @@
+const axios = require('axios')
+const cors = require('cors')
+
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 8000,
+    bodyParser = require('body-parser')
+
+var allowlist = ['http://localhost:4200']
+var corsOptionsDelegate = function (req, callback) {
+    var corsOptions;
+    if (allowlist.indexOf(req.header('Origin')) !== -1) {
+      corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+    } else {
+      corsOptions = { origin: false } // disable CORS for this request
+    }
+    callback(null, corsOptions) // callback expects two parameters: error and options
+  }
+
+app.use(cors(corsOptionsDelegate));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.get('/bypass', async (req,res) => {
+    if(req.query.url === undefined || req.query.url === null){
+        res.send({
+            "error":"error"
+        })
+    }else{
+        try{
+            const data = await axios.get(req.query.url)
+            res.send(data.data)
+        }catch{
+            res.send({
+                "error":"error"
+            })
+        }
+    }
+})
+
+app.listen(port);
+console.log('Bullfrog cors bypass start on : ' + port);
